c01b2c4117f673879f9486ffd8868bcb4ffed18a
dexter-xray <newon.vamos@gmail.com>
2026-06-26 00:30:39 +0300

    Ultimate + dev Xray-core

--- ult_dev.sh
+++ ult_dev.sh
@@ -220,7 +220,7 @@ services:
   remnanode:
     container_name: remnanode
     hostname: remnanode
-    image: remnawave/node:latest
+    image: remnawave/node:dev
     restart: always
     network_mode: host
     environment:
